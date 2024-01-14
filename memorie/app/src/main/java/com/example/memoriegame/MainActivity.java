23d70d26e7297b8f2bb27dd0c02b74eb67441d18
gfigueiredodei <gfigueiredo@student.dei.uc.pt>
2024-01-14 23:41:18 +0000

    limpeza

--- memorie/app/src/main/java/com/example/memoriegame/MainActivity.java
+++ memorie/app/src/main/java/com/example/memoriegame/MainActivity.java
@@ -4,8 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.google.firebase.FirebaseApp;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
--- memorie/app/src/main/java/com/example/memoriegame/fragment_register.java
+++ memorie/app/src/main/java/com/example/memoriegame/fragment_register.java
@@ -22,12 +22,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 import android.widget.Toast;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class fragment_register extends Fragment {
@@ -39,8 +33,6 @@ public class fragment_register extends Fragment {
     Button regist;
 
     Boolean validated_All=false;
-
-    private DatabaseHelper dbHelper;
 
     public static String hashPassword(String password) {
         String saltedPassword = password;
@@ -171,8 +163,6 @@ public class fragment_register extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_register, container, false);
 
-        dbHelper = new DatabaseHelper(requireActivity().getApplicationContext());
-
         return view;
     }
 
@@ -202,7 +192,6 @@ public class fragment_register extends Fragment {
 
             if(validated_All) {
                 createFirebase(userName, email, hashPassword(password));
-                dbHelper.registerUser(email, userName, hashPassword(password));
             }
         });
 
