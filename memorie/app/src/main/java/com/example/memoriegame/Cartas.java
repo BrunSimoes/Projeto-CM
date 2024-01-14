95aa6ce7a99296f0810c911a07986d7894b2b2aa
BrunSimoes <105754146+BrunSimoes@users.noreply.github.com>
2024-01-14 00:34:32 +0000

    cenas

--- memorie/app/src/main/java/com/example/memoriegame/Cartas.java
+++ memorie/app/src/main/java/com/example/memoriegame/Cartas.java
@@ -5,12 +5,17 @@ public class Cartas {
     private int imageId;
     private int id;
     private boolean status;
+    private boolean clicked;
+    private int nCard;
+    private int idPar;
 
     //guardar Status dos elementos a serem dispostos no fragmento Jogo
     public Cartas(int imageId, int id){
         this.status  = true;
         this.imageId = imageId;
         this.id      = id;
+        this.clicked = false;
+        this.idPar    = 0;
     }
 
     //desativar Carta
@@ -21,6 +26,22 @@ public class Cartas {
     //ativar Carta
     public void ativar(){
         this.status = true;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public void setImageId(int imageId){
+        this.imageId = imageId;
+    }
+
+    public void setClicked(boolean clicked){
+        this.clicked = clicked;
+    }
+
+    public void setIdPar(int idPar){
+        this.idPar = idPar;
     }
 
     public boolean obterStatus(){
